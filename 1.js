f331ec296b8eff6c6b8032e1da48221a18387375
ITKaty <117355327+chabanova-ekaterina@users.noreply.github.com>
2023-03-23 16:32:13 +0300

    Add files via upload

--- 1.js
+++ 1.js
@@ -76,6 +76,16 @@ let age_2 = 18;
 let age_3 = 60;
 //   29. Create an if in which you will check the value of the variable age_1
 //   30. If age_1 < age_2, print to console “You don’t have access cause your age is” + age_1 + “It’s less then”
+if (age_1 < age_2){
+  console.log("You don’t have access cause your age is" + age_1  + " It’s less then")
+}
 //   31. If age_1 >= age_2 and age_1 < age_3, output “Welcome!”
+else if (age_1 >= age_2, age_1 < age_3){
+  console.log("Welcome!")
+}
 //   32. If age_1 > age_3, output “Keep calm and look Culture channel” to the console.
+else if (age_1 > age_3){
+  console.log("Keep calm and look Culture channel")
+}
 //   33. Otherwise print "Technical work".
+else ("Technical work")
